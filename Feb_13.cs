dfda689feffdc40675b87d792b919fbe661d0779
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2025-02-13 10:41:12 -0700

    Create Feb_13.cs

---
+++ Feb_13.cs
@@ -0,0 +1,4 @@
+//Today we learned the basics of C#. We learned how to write lines in C#
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+//This is how I make a comment in C#
