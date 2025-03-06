4a31882508fe52ea21ade9070e7c5f85e798f76b
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2025-03-06 10:27:43 -0700

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,6 @@
+// HAPPY Ghana Birthday
+// We learned how to use string, char, int, decimal, and bool for true/false
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("true");
+Console.WriteLine(true);
