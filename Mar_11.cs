4fe26b82a9b7588cced0154fa09c0719c81a9962
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2025-03-11 12:26:23 -0600

    Create Mar_11.cs

---
+++ Mar_11.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+// Module 4 Unit 1 & 2
+//Learned \n,\t,\, \\, @, & \u
+//new line, tab, allows double quotation mark, allows backslash, verbatim string, and special characters
