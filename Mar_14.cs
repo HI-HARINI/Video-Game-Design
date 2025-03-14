48ccce132f0151e86f139dbf74512cc223cb9746
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2025-03-14 08:52:32 -0600

    Create Mar_14.cs

---
+++ Mar_14.cs
@@ -0,0 +1,8 @@
+//Module 4 Unit 3 & 4
+string firstName = "Harini";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+string projectName = "Harini's-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+//$ and @ reduces unecessary code and you can combine variables with + signs in Console.Writeline.
